46f316262c718b73c917678c2ec035c6f4b1c4e9
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-09-23 19:10:49 -0400

    Set up the variables in the Course model and established many-to-one relationship to User model

--- app/models/course.py
+++ app/models/course.py
@@ -8,3 +8,15 @@ user_course = Table('user_course', Base.metadata,
     Column('course_id', Integer, ForeignKey('courses.id'))
 )
 
+class Course(Base):
+    __tablename__ == "courses"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    instructor_id = Column(Integer, ForeignKey('users.id'))
+    
+    # Relationships
+    instructor = relationship("User", back_populates="courses_teaching")
+    students = relationship("User", secondary=user_course, back_populates="courses_enrolled")
+    assignments = relationship("Assignment", back_populates="course")
