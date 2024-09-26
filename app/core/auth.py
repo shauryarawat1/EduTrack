158827400712bdff767e47f58bae83b0fad553b4
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-09-26 00:20:33 -0400

    Set up password functions and token decoder

--- app/core/auth.py
+++ app/core/auth.py
@@ -14,3 +14,21 @@ def create_access_token(data: dict):
     
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
     return encoded_jwt
+
+# Check password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Create hash from plain password
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+# Verify and decode JWT token
+
+def decode_token(token):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    
+    except JWTError:
+        return None
