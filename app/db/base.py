8a04da1082d4885c91b19242682bf9b9e93142da
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-09-25 23:53:12 -0400

    Created config file for auth and updated base database

--- app/db/base.py
+++ app/db/base.py
@@ -1,28 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
-import os
+from app.core.config import settings
 
-# Load environment variables
-load_dotenv()
-
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_SSLMODE = os.getenv("DB_SSLMODE")
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode={DB_SSLMODE}"
-
-# Manage connections to database
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Create SQLAlchemy engine
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
 
 # Base factory to create database connections
-SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create declarative base
-
 Base = declarative_base()
 
 # Dependency to be used in FastAPI routes to get database session
@@ -31,14 +17,13 @@ def get_db():
     try:
         # Ensures that database session is yielded for use in routes
         yield db
-        
     finally:
         # Ensures that database session is closed after each request
         db.close()
-        
+
 # Importing all the models from the folder (User, Course, etc)
 from app.models.user import User
 from app.models.course import Course
 
-# Create tables 
+# Create tables
 Base.metadata.create_all(bind=engine)
