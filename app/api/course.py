6022d566b045541d48b01ac551c86743cb707646
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-09-25 22:19:46 -0400

    Set up API calls for creating and retrieving courses

---
+++ app/api/course.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.crud import course as course_crud
+from app.schemas.course import CourseCreate, Course, CourseUpdate
+from app.db.base import get_db
+
+router = APIRouter()
+
+# Creates a new course
+
+@router.post("/", response_model=Course, status_code=status.HTTP_201_CREATED)
+
+def create_course(
+    course: CourseCreate,
+    db: Session = Depends(get_db)
+):
+    return course_crud.create_course(db = db, course = course, instructor_id=1)
+
+# Retrieves a specific course
+
+@router.get("/{course_id}", response_model=Course)
+
+def read_course(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    courses = course_crud.get_courses(db, skip = skip, limit = limit)
+    return courses
+
+
+
