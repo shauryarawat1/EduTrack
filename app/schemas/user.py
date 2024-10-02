379f4c900a049655fc8ae9b1ffab7a5345699479
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-10-02 16:54:02 -0400

    Updated user schema with role

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,12 +1,18 @@
 from pydantic import BaseModel, EmailStr
+from app.core.security import UserRole
 
 # Fields common to all user schemas
 class UserBase(BaseModel):
     email: EmailStr
+    role: UserRole = UserRole.STUDENT
     
 # For creating new users
 class UserCreate(UserBase):
     password: str
+    
+# For updating users
+class UserUpdate(UserBase):
+    password: str | None = None
     
 # Represents the user as returned by API
 class User(UserBase):
