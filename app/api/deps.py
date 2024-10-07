c7669c170514d63239e78da2fc9ce2eb79ac0664
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-10-07 19:27:16 -0400

    Updated config file

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,7 +1,7 @@
 from typing import Generator
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+import jwt
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
 from app import crud, models, schemas
